290d97f9f50ebfd7fe26afc76a410284ec54c9c9
yiqi-jing <15811025001@163.com>
2025-09-16 21:21:22 +0800

    -

--- Data-collection-and-preprocessing/NO2-lianxi/Untiled.py
+++ Data-collection-and-preprocessing/NO2-lianxi/Untiled.py
@@ -1,2 +1,3 @@
-with open('F:\\Source-code-management-repository\\MyHome\\Data-collection-and-preprocessing\\NO2\\news2.txt', 'r', encoding='utf-8') as f:
-    print(f.readlines())
+with open('F://Source-code-management-repository/MyHome/Data-collection-and-preprocessing/Data/news2.txt', 'r', encoding='GBK') as f:
+    line = f.readline()
+    print(line)
